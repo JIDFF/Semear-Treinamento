3abea24bd91731e1e4fc081afc8139c26441e68f
Jorge <jorgeid@usp.br>
2019-04-29 18:43:16 -0300

    olaaaaaa

--- teste.c
+++ teste.c
@@ -3,12 +3,12 @@
 int main(){
 	printf("Hello\n");
 	printf("fdfdfdfdfHello\n");
-<<<<<<< HEAD
+
 printf("fdfdfdfdfHello\n");
 printf("fdfdfdfdfHello\n");
-=======
+
         printf("fdfdfdfdfHello\n");
         printf("Semear rules!!!!");
->>>>>>> 44f2c67027c75b69d24040d2407a8f3ce162f52d
+
 	return(0);
 }
